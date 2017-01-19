d914a671f3eb0aacdac7325c5b7bfef0ca9be78a
ro9er <ro9er.luo@outlook.com>
2017-01-19 18:22:47 +0800

    add main

--- index.py
+++ index.py
@@ -1,13 +1,15 @@
 import routine
 import hashlib
+import functools
 from wxserv import global_wx_serv
 
 @routine.routine("/")
 def index(signature, timestamp, nonce, echostr):
     list = [global_wx_serv.get_token(), timestamp, nonce]
     list.sort()
+    str_hash = functools.reduce(lambda a,b: a + b, list, "")
     sha1 = hashlib.sha1()
-    map(sha1.update, list)
+    sha1.update(str_hash.encode("utf-8"))
     hashcode = sha1.hexdigest()
     print("handle/GET func: hashcode, signature: ", hashcode, signature)
     if hashcode == signature:
